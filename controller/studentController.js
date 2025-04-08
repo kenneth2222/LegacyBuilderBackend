d4492b3071d10f4dc43470e7557751da40d9e5a4
kenneth2222 <obusco4lyfe@gmail.com>
2025-04-08 22:34:30 +0100

    latest commit

--- controller/studentController.js
+++ controller/studentController.js
@@ -605,6 +605,9 @@ exports.updateImage = async (req, res) => {
         message: "Student ID is required",
       });
     }
+    if (!req.file) {
+      return res.status(400).json({ message: "Image is required" });
+    }
     
     const student = await studentModel.findById(studentId);
 
@@ -614,24 +617,6 @@ exports.updateImage = async (req, res) => {
       });
     }
 
-    if (student.image && student.image.public_id) {
-      
-      await cloudinary.uploader.destroy(student.image.public_id);
-
-
-    if (!req.file) {
-      return res.status(400).json({
-        message: "Image is required",
-      });
-    }
-
-    const student = await studentModel.findById(studentId);
-
-    if (!student) {
-      return res.status(404).json({
-        message: "Student not found",
-      });
-    }
 
     // Delete the previous image from Cloudinary
     if (student.image && student.image.public_id) {
@@ -646,8 +631,12 @@ exports.updateImage = async (req, res) => {
 
     // Compress the image
     await sharp(req.file.path)
-      .jpeg({ quality: 70 })
-      .toFile(compressedFilePath);
+    .resize(60, 60, {
+      fit: sharp.fit.inside,
+      withoutEnlargement: true,
+    })
+    .jpeg({ quality: 70 })
+    .toFile(compressedFilePath);
 
    
       const result = await cloudinary.uploader.upload(compressedFilePath, {
@@ -655,11 +644,6 @@ exports.updateImage = async (req, res) => {
         use_filename: true,
       });
     
-
-    // Delete the local file after upload
-    // fs.unlink(req.file.path, (err) => {
-    //   if (err) console.log("Error deleting local image:", err.message);
-    // });
     fs.unlink(req.file.path)
     .then(() => {
       console.log("File deleted successfully");
