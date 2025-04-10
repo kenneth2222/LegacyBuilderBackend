fd9cb847b41bb0dbb0adb29453b1426486e64706
kenneth2222 <obusco4lyfe@gmail.com>
2025-04-10 12:07:29 +0100

    render1

--- controller/studentController.js
+++ controller/studentController.js
@@ -23,11 +23,6 @@ const {
   resetStudentPasswordSchema,
 } = require("../middleware/validator");
 
-// cloudinary.config({
-//   cloud_name: process.env.CLOUD_NAME,
-//   api_key: process.env.CLOUD_API_KEY,
-//   api_secret: process.env.CLOUD_API_SECRET
-// });
 
 exports.registerStudent = async (req, res) => {
   try {
